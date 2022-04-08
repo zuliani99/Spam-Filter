f712cc601e91593f8e1cf1e2a9a73e3670987708
Luca Fortin <fortluca96@gmail.com>
2022-04-08 17:35:31 +0200

    add shuffle

--- solve.py
+++ solve.py
@@ -1,7 +1,9 @@
 import pandas as pd
+import numpy as np
 
 def readData():
     data = pd.read_csv("spambase.data", "," ,index_col = False)
+    np.random.shuffle(data)
     return data.iloc[:, :54], data.iloc[:,57]
 
 if __name__ == "__main__":
