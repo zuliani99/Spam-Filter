3130385da168e7289819bf36ee289e53433d79e0
Luca Fortin <fortluca96@gmail.com>
2022-04-12 11:43:53 +0200

    Creazione classe Svm
    Creazione classe Svm con relative funzioni + invocazione della stessa nel main

---
+++ classifiers/svm_classifier.py
@@ -0,0 +1,68 @@
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split, cross_val_predict, cross_val_score
+
+RESULT = "files/result.txt"
+CV = 10
+KERNELS = ["linear", "poly", "rbf"]
+svm_list = []
+svm_angular_kernel_list = []
+
+class Svm:
+
+    def __init__ (self, X, Y):
+        self.X = X
+        self.Y = Y
+
+    # calculate the term frequency of each word in each mail
+    def tf_idf(self):
+        idf = np.log10(self.X.shape[0] / (self.X != 0).sum(0))
+        return self.X / 100.0 * idf
+
+    # evaluate kernel and return mean accuracy, variance and standard variation
+    def evaluation_kernel(self, kernel):
+        if kernel == "poly":
+            classifier = SVC(kernel=kernel, degree=2, C=1.0)
+            svm_list.append(classifier)
+        else:
+            classifier = SVC(kernel=kernel, C=1.0)
+            svm_list.append(classifier)
+
+        ten_way_CV_score = cross_val_score(classifier, self.X, self.Y, cv=CV, n_jobs=-1)
+        mean = ten_way_CV_score.mean()
+        var = ten_way_CV_score.var()
+        std = ten_way_CV_score.std()
+
+        return [str(mean), str(var), str(std)]
+
+    def classifier_score(classifier, x_train, y_train):
+        fit = classifier.fit(x_train, y_train)
+        return fit.score(x_train, y_train)
+
+    def result(self):
+        file = open(RESULT, "w")
+        file.write("SVM CLASSIFIER \n")
+        file.write("\n")
+
+        # kernels without angular kernel evaluation
+        file.write("Measures of kernels without angular kernel evaluation: \n")
+        for k in KERNELS:
+            results = self.evaluation_kernel(k)
+            write_result(file, k, results)
+
+        file.write("\n")
+
+        # kernels with angular kernel evaluation
+        file.write("Measures of kernels with angular kernel evaluation: \n")
+        for k in KERNELS:
+            results = self.evaluation_kernel(k)
+            write_result(file, k, results)
+
+            
+
+def write_result(file, kernel, results):
+    file.write("\n")
+    file.write("---- " + kernel + " kernel "+" ---- \n")
+    file.write("Mean Accuracy: " + str(results[0])+"\n")
+    file.write("Variance Accuracy: " + str(results[1])+"\n")
+    file.write("Standard Variation Accuracy: " +str(results[2])+"\n")
--- solve.py
+++ solve.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import numpy as np
+from classifiers.svm_classifier import Svm 
 
 def readData():
-    data = pd.read_csv("spambase.data", "," ,index_col = False)
-    np.random.shuffle(data)
+    data = pd.read_csv("files/spambase.data", "," ,index_col = False)
     return data.iloc[:, :54], data.iloc[:,57]
 
 if __name__ == "__main__":
-    X, Y = readData()
+    X,Y = readData()
     #SVM
+    svm = Svm(X,Y)
+    svm.result()
     #NAIVE BAYES
     #KNN
